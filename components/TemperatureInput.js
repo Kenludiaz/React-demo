8d99ef2f6f7ee5c24d290fc5c0b9fb7f0bca1f43
Kenneth Lu Diaz <ludiazken@gmail.com>
2020-10-27 12:47:49 -0400

    Lifting state up.

--- components/TemperatureInput.js
+++ components/TemperatureInput.js
@@ -1,17 +1,24 @@
-class Calculator extends React.Component {
-    constructor(props) {
-      super(props);
-      this.handleChange = this.handleChange.bind(this);
-      this.state = {temperature: ''};  }
-  
-    handleChange(e) {
-      this.setState({temperature: e.target.value});  }
-  
-    render() {
-      const temperature = this.state.temperature;    return (
-        <fieldset>
-          <legend>Enter temperature in Celsius:</legend>
-          <input          value={temperature}          onChange={this.handleChange} />        <BoilingVerdict          celsius={parseFloat(temperature)} />      </fieldset>
-      );
-    }
+import React from 'react';
+
+class TemperatureInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange(e) {
+    this.props.onTemperatureChange(e.target.value);  }
+
+  render() {
+    const temperature = this.props.temperature;    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}:</legend>
+        <input value={temperature}
+               onChange={this.handleChange} />
+      </fieldset>
+    );
+  }
+}
+
+export default TemperatureInput
